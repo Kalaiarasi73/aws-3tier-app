8c912f5c901cf1da58ae9c73b81cb59a7f4cedf0
Kalaiarasi <147124339+Kalaiarasi73@users.noreply.github.com>
2026-02-25 00:00:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 from flask import Flask
-import boto3
 
 app = Flask(__name__)
-
-dynamodb = boto3.resource('dynamodb', region_name='ap-south-1')
-table = dynamodb.Table('users')
 
 @app.route('/')
 def home():
     return "Hello! My AWS 3-Tier App is Running!"
 
 @app.route('/users')
-def get_users():
-    result = table.scan()
-    return str(result)
+def users():
+    return "Users page is working!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
